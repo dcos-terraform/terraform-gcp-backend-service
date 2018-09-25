b66e47953938b81122988a39ea6d34894dbc81e6
Lukas Loesche <lukas@mesosphere.io>
2018-09-25 14:03:39 +0200

    Automatic update of variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,28 +1,42 @@
 # project id
-variable "project_id" {}
+variable "project_id" {
+  description = "project id"
+}
 
 # region
-variable "region" {}
+variable "region" {
+  description = "region"
+}
 
 # Cluster Name
-variable "name_prefix" {}
+variable "name_prefix" {
+  description = "Cluster Name"
+}
 
 # Network Name
-variable "network" {}
+variable "network" {
+  description = "network"
+}
 
 # Target Pool
-variable "target_pool" {}
+variable "target_pool" {
+  description = "target pool"
+}
 
 # Instances Self Link
 variable "instances_self_link" {
+  description = "List of instance self links"
   type = "list"
 }
 
 # ports to allow
 variable "allow_ports" {
+  description = "allow ports"
   type    = "list"
   default = ["5050", "2181", "8181", "8080"]
 }
 
 # DCOS Role
-variable "dcos_role" {}
+variable "dcos_role" {
+  description = "dcos role"
+}
