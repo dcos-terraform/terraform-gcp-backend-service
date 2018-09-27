7bd9c3d5ee8cc8a8356f5dba9b516bc874bfcbd2
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:55:59 +0200

    Automatic update of variable descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,29 @@
 # Cluster Name
 output "name_prefix" {
+  description = "Cluster Name"
   value = "${var.name_prefix}"
 }
 
 # Network Name
 output "network" {
+  description = "network"
   value = "${var.network}"
 }
 
 # Target Pool Name
 output "target_pool" {
+  description = "target pool"
   value = "${var.target_pool}"
 }
 
 # Instances Self Link
 output "instances_self_link" {
+  description = "List of instance self links"
   value = "${var.instances_self_link}"
 }
 
 # DCOS Role
 output "dcos_role" {
+  description = "dcos role"
   value = "${var.dcos_role}"
 }
